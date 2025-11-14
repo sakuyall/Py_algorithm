7f9ad5973ea9b428eccf5f1723b7739c3d5a2ce9
森の白黒ネズミ <joytin525@outlook.com>
2025-11-14 13:45:41 +0800

    remastered@11/14/25

--- p0_算法与数据结构/p0d24_二叉搜索树.py
+++ p0_算法与数据结构/p0d24_二叉搜索树.py
@@ -1,10 +1,23 @@
-"""11/13/25
+"""11/13-14/25
 二叉搜索树
     首先它是一棵二叉树
     其次左子树及其下属都比根节点小,右子树及其下属都比根节点大
 
 查找,插入
     查找和插入的性质相差不大,按深度搜索
+
+删除
+    删除叶子节点-直接删除
+    删除有两个孩子的节点-将其右子树最小节点(该节点最多有一个右子节点)删除并替换到当前位置
+    用左边最大的也行(因为要保证二叉搜索树的定义)
+
+效率
+    平均情况下二叉搜索树的搜索时间复杂度为O(nlogn)
+    可能存在最坏情况二叉树全为同一方向的子节点(偏斜)
+
+解决方案
+    随机化插入
+    AVL树(见下节)
 """
 class BiTreeNode:
     def __init__(self, data):
@@ -63,6 +76,25 @@ class BST:
             else:
                 return
     
+    def query(self, node, val):    # 查找
+        if not node:
+            return None
+        elif node.data < val:
+            return self.query(node.lchild, val)
+        elif node.data > val:
+            return self.query(node.rchild, val)
+    
+    def query_no_rec(self, val):
+        p = self.root
+        while p:
+            if p.data < val:
+                p = p.rchild
+            elif p.data > val:
+                p = p.lchild
+            else:
+                return p
+        return None     # 找不到返回空
+
     def pre_order(self, root):
         if root:  # root非空
             print(root.data, end = ",") # 根左右
@@ -81,15 +113,89 @@ class BST:
             self.post_order(root.rchild)
             print(root.data, end = ",")
 
+    def __remove_node_1(self, node):
+        # 情况一 node是叶子节点的情况
+        if not node.parent:
+            # node的parent为空时(说明是根节点)
+            self.root = None
+        if node == node.parent.lchild:
+            # 若node是其父节点的左子节点
+            node.parent.lchild = None
+        elif node == node.parent.rchild:
+            # 若node是其父节点的右子节点
+            node.parent.rchild = None
+    
+    def __remove_node_21(self, node):
+        # 情况21 node只有一个左子节点
+        if not node.parent:
+            # 是根节点就把它唯一的子节点上位作为根
+            self.root = node.lchild
+            node.lchild.parent = None    # 将左子节点升为根节点(父节点None)
+        elif node == node.parent.lchild:
+            # 若node是其父节点的左子节点
+            node.parent.lchild = node.lchild    # node父节点与node左子节点连接
+            node.lchild.parent = node.parent    # 孙子把爷爷当父亲
+        else:
+            # 若node是其父节点的右子节点
+            node.parent.rchild = node.lchild    # 一个道理
+            node.lchild.parent = node.parent
+        
+    def __remove_node_22(self, node):
+        # 情况22 node只有一个右子节点
+        if not node.parent:
+            self.root = node.rchild
+        elif node == node.parent.lchild:
+            node.parent.lchild = node.rchild
+            node.rchild.parent = node.parent
+        else:
+            node.parent.rchild = node.rchild
+            node.rchild.parent = node.parent
+
+    def delete(self, val):
+        if self.root:
+            # 首先判断不是空树,再开始删除
+            node = self.query_no_rec(val)
+            if not node:
+                # 87行找不到对应元素就返回空
+                # 此时返回False
+                raise ValueError("查找的值在树中不存在")
+        if not node.lchild and not node.rchild:
+            # 左右子节点都为空
+            self.__remove_node_1(node)
+        elif not node.rchild:
+            # 仅有左子节点
+            self.__remove_node_21(node)
+        elif not node.rchild:
+            # 仅有右子节点
+            self.__remove_node_22(node)
+        else:
+            # 两个都有,找右子树最小节点出来,找左边最大也行
+            min_node = node.rchild
+            # 也就是进入根节点右子树,重复寻找左子节点的左子节点直至没有位置
+            while min_node.lchild:
+                min_node = min_node.lchild
+            # 将获取到值的覆盖到当前node位置
+            node.data = min_node.data
+            # 接着在原位置删除min_node(它没有左子节点)执行删除函数
+            # 所以它要么是叶子节点,要么还有一个右子节点
+            if min_node.rchild:
+                # 若存在右子节点
+                self.__remove_node_22(min_node)
+            else:
+                self.__remove_node_1(min_node)
+
 import random
 li = list(range(100))
 random.shuffle(li)
 
 # tree = BST([4, 6, 7, 9, 2, 1, 3, 5, 8])
-tree = BST(li)
-tree.pre_order(tree.root)   # 前序返回4,2,1,3,6,5,7,9,8,
-print("")
+# tree = BST(li)
+tree = BST([1, 4, 2, 5, 3, 8, 6, 9, 7])
+# tree.pre_order(tree.root)   # 前序返回4,2,1,3,6,5,7,9,8,
+# print("")
 tree.in_order(tree.root)    # 中序返回1,2,3,4,5,6,7,8,9,
 print("")                   # 二叉搜索树的中序序列按照左中右能够按升序输出
-tree.post_order(tree.root)  # 后序返回1,3,2,5,8,9,7,6,4,
-print("")
+# tree.post_order(tree.root)  # 后序返回1,3,2,5,8,9,7,6,4,
+# print("")
+tree.delete(4)
+tree.in_order(tree.root)   # 返回1,2,3,5,6,7,8,9,
